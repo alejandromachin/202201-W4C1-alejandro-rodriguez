3cf31b4b8a90887c14020c463889ace249204f69
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-31 20:08:27 +0100

    modified info function to recieve a value an render it

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,9 +1,11 @@
 const Info = ({ numberOfMarkedGentelmen }) => {
   return (
     <>
-      <section class="controls">
-        <p class="info">0 gentlemen pointing at you</p>
-        <button class="button button--select">Select all</button>
+      <section className="controls">
+        <p className="info">
+          {numberOfMarkedGentelmen} gentlemen pointing at you
+        </p>
+        <button className="button button--select">Select all</button>
       </section>
     </>
   );
