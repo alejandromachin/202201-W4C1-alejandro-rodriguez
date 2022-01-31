fa906d645dd75e4243866a922213ec88413a38ce
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-31 22:16:46 +0100

    added all properties of gentelman object to the info in jsx

--- src/components/Gentelmen/Gentelmen.js
+++ src/components/Gentelmen/Gentelmen.js
@@ -16,23 +16,23 @@ const Gentelmen = ({ gentelman }) => {
         <div className="gentleman__avatar-container">
           <img
             className="gentleman__avatar"
-            src="/images/fary.jpg"
-            alt="The Fary pointing at you"
+            src={`images/${picture}`}
+            alt={alternativeText}
           />
           <span className="gentleman__initial">F</span>
         </div>
         <div className="gentleman__data-container">
-          <h2 className="gentleman__name">The Fary</h2>
+          <h2 className="gentleman__name">{name}</h2>
           <ul className="gentleman__data-list">
             <li className="gentleman__data">
               <span className="gentleman__data-label">Profession:</span>
-              Influencer
+              {profession}
             </li>
             <li className="gentleman__data">
-              <span className="gentleman__data-label">Status:</span> RIP
+              <span className="gentleman__data-label">Status:</span> {status}
             </li>
             <li className="gentleman__data">
-              <span className="gentleman__data-label">Twitter:</span> Pending
+              <span className="gentleman__data-label">Twitter:</span> {twitter}
             </li>
           </ul>
         </div>
