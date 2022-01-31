998be294637c31b3b61f25d1307f942d9f5189e4
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-31 19:42:21 +0100

    added info component with basic jsx

---
+++ src/components/Info/Info.js
@@ -0,0 +1,12 @@
+const Info = ({ numberOfMarkedGentelmen }) => {
+  return (
+    <>
+      <section class="controls">
+        <p class="info">0 gentlemen pointing at you</p>
+        <button class="button button--select">Select all</button>
+      </section>
+    </>
+  );
+};
+
+export default Info;
